ddc2e34f0b86dd2af7c7c6897d37a7201f5f03e9
Xavier Silva <xavier@DESKTOP-2FQOQ98.>
2024-10-14 17:59:08 +0100

    finale

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
-#include "Proj/WaterSupply.h"
+#include "Proj/WaterSupply.h" // Include the definition of the WaterSupply class.
+#include "Proj/Menu.h" // Include the definition of the Menu class.
 
-//
-// Created by Migas on 3/21/2024.
-//
 int main(){
-    WaterSupply ws= WaterSupply();
+    WaterSupply ws; // Create an instance of WaterSupply to manage the water supply network.
+    Menu menu; // Create an instance of Menu to handle user interactions.
 
-    return 0;
+    menu.chooseDataSet(&ws); // Prompt the user to choose a data set for the water supply network.
+
+    ws = WaterSupply(); // Re-initialize ws to reset or start with a new WaterSupply instance.
+    ws.buildNetwork(); // Build the network graph based on the data set chosen or default data.
+
+    ws.get_MaxFlow(cityF, ws); // Calculate the maximum flow from sources to sinks in the network.
+
+    menu.mainMenu(ws); // Display the main menu of the application for further user interactions.
+
+    return 0; // End the program.
 }
